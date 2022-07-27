cdcdc41afc6b9f7386ded63523afc0498e62aa79
Glitch (the-one-who-only-observes) <none>
2022-07-27 04:59:20 +0000

    added vote command

--- src/events/interactionCreate.js
+++ src/events/interactionCreate.js
@@ -1,18 +1,16 @@
 const config = require("../config.json");
 
 module.exports = async (client, interaction) => {
+  if (!interaction.isChatInputCommand()) return;
   await interaction.deferReply();
 
-  if (interaction.isCommand()) {
-    const command = client.commands.get(interaction.commandName);
-    if (!command)
-      return interaction.editReply("Command unavailable ¯\\_(ツ)_/¯");
+  const command = client.commands.get(interaction.commandName);
+  if (!command) return interaction.editReply("Command unavailable ¯\\_(ツ)_/¯");
 
-    if (!config.ownerId.includes(interaction.user.id))
-      return interaction.editReply(
-        "You are not authorized to do so! ¯\\_(ツ)_/¯"
-      );
+  if (!config.ownerId.includes(interaction.user.id))
+    return interaction.editReply(
+      "You are not authorized to do so! ¯\\_(ツ)_/¯"
+    );
 
-    await command.code(client, interaction);
-  }
+  await command.code(client, interaction);
 };
--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ const app = express();
 (async () => {
   app.use(express.json());
   app.use(express.urlencoded({ extended: false }));
-  app.get("/", (req, res) => res.status(200)); // receive pings
+  app.get("/", (req, res) => res.sendStatus(200)); // receive pings
 
   await require("./webhooks/")(app);
 
