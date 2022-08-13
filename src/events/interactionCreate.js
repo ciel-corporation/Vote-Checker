c863c7d5f5ca2f993bbce56823f06ae5e45fc068
Haruka Yamamoto <87913906+HarukaYamamoto0@users.noreply.github.com>
2022-08-13 13:18:18 +0000

    backup

--- src/events/interactionCreate.js
+++ src/events/interactionCreate.js
@@ -1,19 +1,18 @@
-const config = require("../config.json");
+const { PermissionsBitField } = require("discord.js");
 
 module.exports = async (client, interaction) => {
-  if (!interaction.isChatInputCommand()) return;
-  await interaction.deferReply();
+	if (!interaction.isChatInputCommand()) return;
+	await interaction.deferReply();
 
-  const command = client.commands.get(interaction.commandName);
-  if (!command) return interaction.editReply("Command unavailable ¯\\_(ツ)_/¯");
+	const command = client.commands.get(interaction.commandName);
+	if (!command) return interaction.editReply("Command unavailable ¯\\_(ツ)_/¯");
 
-  if (false) {
-    return interaction.editReply("You don't have enough permission for this!");
-  }
-  if (command.isOwner && !config.ownerIds.includes(interaction.user.id))
-    return interaction.editReply(
-      "You are not authorized to do so! ¯\\_(ツ)_/¯"
-    );
+	if (
+		interaction.commandName === "setchannel" &&
+		!interaction.member.permissions.has(PermissionsBitField.Flags.ManageChannels)
+	) {
+		return interaction.editReply("You don't have enough permission for this!");
+	}
 
-  await command.code(client, interaction);
+	await command.code(client, interaction);
 };
