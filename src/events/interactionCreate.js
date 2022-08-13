9b680f2bbd67603576b1d3c82b0adbc07f5e529f
Glitch (near-voltaic-coneflower) <none>
2022-08-13 14:35:02 +0000

    backup

--- src/events/interactionCreate.js
+++ src/events/interactionCreate.js
@@ -1,4 +1,4 @@
-const config = require("../config.json");
+const { PermissionsBitField } = require("discord.js");
 
 module.exports = async (client, interaction) => {
   if (!interaction.isChatInputCommand()) return;
@@ -7,13 +7,14 @@ module.exports = async (client, interaction) => {
   const command = client.commands.get(interaction.commandName);
   if (!command) return interaction.editReply("Command unavailable ¯\\_(ツ)_/¯");
 
-  if (false) {
+  if (
+    interaction.commandName === "setchannel" &&
+    !interaction.member.permissions.has(
+      PermissionsBitField.Flags.ManageChannels
+    )
+  ) {
     return interaction.editReply("You don't have enough permission for this!");
   }
-  if (command.isOwner && !config.ownerIds.includes(interaction.user.id))
-    return interaction.editReply(
-      "You are not authorized to do so! ¯\\_(ツ)_/¯"
-    );
 
   await command.code(client, interaction);
 };
